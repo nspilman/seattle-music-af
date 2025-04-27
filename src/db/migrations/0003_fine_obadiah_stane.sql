5a699fff0142a5bbb95ad9813bc5732a7dc74a41
Nate Spilman <nate.spilman@gmail.com>
2025-04-27 13:27:47 -0700

    making ranking questions required

---
+++ src/db/migrations/0003_fine_obadiah_stane.sql
@@ -0,0 +1,8 @@
+ALTER TABLE "survey_submissions" ALTER COLUMN "website_rating" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "survey_submissions" ALTER COLUMN "epk_rating" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "survey_submissions" ALTER COLUMN "resource_dir_rating" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "survey_submissions" ALTER COLUMN "fan_rating" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "survey_submissions" ALTER COLUMN "marketing_rating" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "survey_submissions" ALTER COLUMN "merch_rating" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "survey_submissions" ALTER COLUMN "booking_rating" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "survey_submissions" ALTER COLUMN "analytics_rating" SET NOT NULL;
